8937e86a5cdaaaca2f4a4d02433b14c466d7dffc
Takuma <takustudio@icloud.com>
2026-02-12 00:13:58 +0900

    feat: implement Layout class for record management

---
+++ easy-peasy-db/src/main/java/esypsydb/record/Layout.java
@@ -0,0 +1,45 @@
+package esypsydb.record;
+
+import static java.sql.Types.*;
+
+import esypsydb.file.Page;
+import esypsydb.record.Schema;
+import java.util.*;
+
+public class Layout {
+    private Schema schema;
+    private Map<String, Integer> offsets;
+    private int slotsize;
+
+    public Layout(Schema schema) {
+        this.schema = schema;
+        offsets = new HashMap<>();
+        int pos = Integer.BYTES;
+        for (String fldname : schema.fields()) {
+            offsets.put(fldname, pos);
+            pos += lengthInBytes(fldname);
+        }
+        slotsize = pos;
+    }
+    public Layout(Schema schema, Map<String, Integer> offsets, int slotsize) {
+        this.schema = schema;
+        this.offsets = offsets;
+        this.slotsize = slotsize;
+    }
+
+    public Schema schema() {
+        return schema;
+    }
+
+    public int slotsize() {
+        return slotsize;
+    }
+
+    private int lengthInBytes(String fldname) {
+        int fldtype = schema.type(fldname);
+        if (fldtype == INTEGER)
+            return Integer.BYTES;
+        else // fldtype == VARCHAR
+            return Page.maxLength(schema.length(fldname));
+    }
+}
