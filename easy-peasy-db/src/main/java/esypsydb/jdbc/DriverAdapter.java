f135103a0ebe2915c0d4b6e471f0384dcaa202de
Takuma <takustudio@icloud.com>
2026-03-02 00:38:09 +0900

    feat: implement abstract JDBC adapter classes

---
+++ easy-peasy-db/src/main/java/esypsydb/jdbc/DriverAdapter.java
@@ -0,0 +1,35 @@
+package esypsydb.jdbc;
+
+import java.sql.*;
+import java.util.*;
+import java.util.logging.Logger;
+
+public abstract class DriverAdapter implements Driver {
+    public boolean acceptsURL(String url) throws SQLException {
+        throw new SQLException("operation not imolemented");
+    }
+
+    public Connection connect(String url, Properties info) throws SQLException {
+        throw new SQLException("operation not implemented");
+    }
+
+    public int getMajorVersion() {
+        return 0;
+    }
+
+    public int getMinorVersion() {
+        return 0;
+    }
+
+    public DriverPropertyInfo[] getPropertyInfo(String url, Properties info) {
+        return null;
+    }
+
+    public boolean jdbcCompilant() {
+        return false;
+    }
+
+    public Logger getPaLogger() throws SQLFeatureNotSupportedException {
+        throw new SQLFeatureNotSupportedException("operation not implemented");
+    }
+}
