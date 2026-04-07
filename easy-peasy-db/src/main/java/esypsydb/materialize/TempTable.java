af5983625f4de0f4059eea7cdda2e2c04611c83b
Takuma <takustudio@icloud.com>
2026-04-07 12:08:48 +0900

    feat: add TempTable class

---
+++ easy-peasy-db/src/main/java/esypsydb/materialize/TempTable.java
@@ -0,0 +1,41 @@
+package esypsydb.materialize;
+
+import esypsydb.record.*;
+import esypsydb.query.*;
+import esypsydb.tx.Transaction;
+
+public class TempTable {
+    private static int nextTableNum = 0;
+    private Transaction tx;
+    private String tblname;
+    private Layout layout;
+
+    public TempTable(Transaction tx, Schema sch) {
+        this.tx = tx;
+        tblname = nextTableNum();
+        layout = new Layout(sch);
+    }
+
+    public UpdateScan open() {
+        return new TableScan(tx, tblname, layout);
+    }
+
+    public String tablename() {
+        return tblname;
+    }
+
+    public Layout getLayout() {
+        return layout;
+    }
+
+    /*
+     * 一意な名前tempNをつける
+     */
+    public static synchronized String nextTableNum() {
+        nextTableNum++;
+        return "temp" + nextTableNum;
+    }
+
+ 
+    
+}
